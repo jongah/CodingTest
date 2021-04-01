62a66735eb118a95c26232077f12befcdde85f7f
JongAh_Gang <jongah2021@gmail.com>
2021-04-01 13:35:28 +0000

    1202 modify input()

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 
+n, k = map(int, input().split())
+jewel = []
+b = []
+for i in range(n):
+  a = list(map(int, input().split()))
+  jewel.append(a)
+for i in range(k):
+  b.append(int(input()))
 
+b.sort()
+result = 0
+passIndex = [-1]*k
 
+for i in range(k):
+  count = 0
+  for j in range(n):
+    if jewel[j][0] <= b[i] and j not in passIndex:
+      if jewel[j][1] > count:
+        count = jewel[j][1]
+        passIndex[i] = j
+  result += count
+
+print(result)
+  #작은 가방부터 나열한다.
+  #가방에 들어갈 수 있는 무게의 보석 중 가장 비싼 보석을 추가 한다.
--- 그리디&구현/1202_보석도둑.py
+++ 그리디&구현/1202_보석도둑.py
@@ -1,10 +1,24 @@
-def JewelThief():
-  n = 3
-  k = 2
-  jewel = [[1, 65], [5, 23], [2, 99]]
-  b = [10, 2]
+# n, k = map(int, input().split())
+# jewel = []
+# b = []
+# for i in range(n):
+#   a = list(map(int, input().split()))
+#   jewel.append(a)
+# for i in range(k):
+#   b.append(int(input()))
 
-  # 입력 완료 했다 치고 여기서 부터 계산
+# print(jewel, b)
+
+def JewelThief():
+  n, k = map(int, input().split())
+  jewel = []
+  b = []
+  for i in range(n):
+    a = list(map(int, input().split()))
+    jewel.append(a)
+  for i in range(k):
+    b.append(int(input()))
+
   b.sort()
   result = 0
   passIndex = [-1]*k
@@ -13,12 +27,11 @@ def JewelThief():
     count = 0
     for j in range(n):
       if jewel[j][0] <= b[i] and j not in passIndex:
-        print(jewel[j][1], count)
         if jewel[j][1] > count:
           count = jewel[j][1]
           passIndex[i] = j
-          print(count)
     result += count
+    
   print(result)
   #작은 가방부터 나열한다.
   #가방에 들어갈 수 있는 무게의 보석 중 가장 비싼 보석을 추가 한다.
