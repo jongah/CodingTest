e8ba38332a0587df15e0e22b777f638351b2f8b6
JongAh_Gang <80739992+jongah@users.noreply.github.com>
2021-04-13 14:18:06 +0900

    Update 완주하지 못한 선수.py

--- 프로그래머스/Level_1/완주하지 못한 선수.py
+++ 프로그래머스/Level_1/완주하지 못한 선수.py
@@ -2,15 +2,16 @@
 #만약 끝까지 나오지 않는다면 participant의 마지막 값을 리턴
 
 def solution(participant, completion):
-    participant.sort()
+    participant.sort() #정렬
     completion.sort()
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
+    for i in range(len(completion)): #completion만큼 돌린다. 이때 participant를 넣으면
+        if participant[i] != completion[i]: #여기서 리스트 범위 에러난다.
             answer = participant[i]
-            return answer
-    return participant[-1]
+            return answer #더 돌지 말고 바로 리턴 시킨다.
+    return participant[-1] #다 돌았는데 일치한다면 participant의 마지막 값이 답
   
 print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
+
 
 #다른사람 풀이
 import collections
